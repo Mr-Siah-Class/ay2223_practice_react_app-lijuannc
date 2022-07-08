aaa3a64d85db4013ca17bf65db02363fc13239be
Li Juan <clijuan.21@ichat.sp.edu.sg>
2022-07-08 15:22:45 +0800

    Add New Screens #4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './App.css';
 
-import NavBar from './components/NavBar';
+import {
+    Route,
+    useParams,
+    Routes,
+    BrowserRouter
+  } from "react-router-dom";
+  
+  import NavBar from './components/NavBar';
+  import HomeScreen from './screens/HomeScreen';
+  import ProductsScreen from './screens/ProductsScreen';
+  import NoScreen from './screens/NoScreen';
+  import FooterBar from './components/FooterBar';
+  import SingleProductScreen from './screens/SingleProductScreen';
+  
 
 
 class App extends React.Component
@@ -14,7 +27,18 @@ class App extends React.Component
     {
         return (
             <div>
-                <h1>Hello React!</h1>
+                <NavBar/>
+
+                <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<SingleProductScreen/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+                
+                <FooterBar/>
             </div>
         );
     }
--- src/screens/NoScreen.js
+++ src/screens/NoScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class HomeScreen extends React.Component
+class NoScreen extends React.Component
 {
     constructor() {
         super()
@@ -10,10 +10,10 @@ class HomeScreen extends React.Component
     {
         return (
             <div className='Main'>
-                <h1>HomeScreen</h1>
+                <h1>404 Not Found</h1>
             </div>
         );
     }
 }
 
-export default HomeScreen;
+export default NoScreen;
